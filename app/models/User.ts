25a2eaab0a7ca94c1391ffe4afd9172d3ec19b4c
Omer Faruk Coskun <ofcskn.omer@gmail.com>
2024-11-02 21:13:02 -0400

    Update user model

--- app/models/User.ts
+++ app/models/User.ts
@@ -1,5 +1,8 @@
+import { Double } from "react-native/Libraries/Types/CodegenTypes";
+
 export interface User {
     _id: string;   // Unique identifier for the user (string type)
     name: string;  // User's name (string type)
     email: string; // User's email address (string type)
+    balance: Double // Balance of the user in the bank
 }
