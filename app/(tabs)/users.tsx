2f77ca9f607b69252938b663460fd3583d81ab3e
Omer Faruk Coskun <ofcskn.omer@gmail.com>
2024-11-02 20:34:09 -0400

    Create a User

--- app/(tabs)/users.tsx
+++ app/(tabs)/users.tsx
@@ -1,18 +1,35 @@
-import { Image, StyleSheet, Platform, SectionList } from 'react-native';
+import { Image, StyleSheet, Platform, SectionList, Button, Modal, View, TextInput } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { FlatList } from 'react-native-gesture-handler';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import config from '../config/config';
 import { User } from '../models/User';
-
+import { useForm, Controller } from 'react-hook-form';
+import { AnimatedView } from 'react-native-reanimated/lib/typescript/reanimated2/component/View';
 
 export default function HomeScreen() {
     const [data, setData] = useState<User[]>();
+    const [createUserModalVisible, setCreateUserModalVisible] = useState(false);
+
+    // Creating a user
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+  
+  const handleSubmit = (e:any) =>  {
+        const response = axios.post(`${config.apiUrl}/users/create`, {"name": name, "email": email})
+        .then((response) => {
+          data?.push(response.data);
+          setCreateUserModalVisible(false);
+        }).catch((error)=> {
+          console.log("Error:", error);
+        });
+        
+    }
 
     useEffect(() => {
         axios.get(`${config.apiUrl}/users`)
@@ -28,14 +45,55 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
+        <Button
+  title="Create a User"
+  onPress={() => setCreateUserModalVisible(true)}
+  color="#333"
+  accessibilityLabel="Create a user that has access the banking system"
+/>
      <ThemedView style={styles.stepContainer}>
         <FlatList
             data={data}
             renderItem={({item}) => <ThemedText> {item.name} - {item.email}</ThemedText>}
             keyExtractor={item => item._id}
         />
+        <Modal
+  animationType="slide"
+  transparent={false}
+  visible={createUserModalVisible}
+  onRequestClose={() => {
+    setCreateUserModalVisible(!createUserModalVisible);
+  }}
+>
+  <ScrollView>
+    <View>
+      <ThemedText style={{"color": "#000"}} type='title' darkColor=''>Create a User</ThemedText>
+    </View>
+      <TextInput
+          style={styles.input}
+          onChangeText={(data) => setName(data)}
+          value={name}
+          placeholder="Name"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={(data) => setEmail(data)}
+          value={email}
+          placeholder="Email"
+        />
+        <View style={{"marginBottom": 10}}>
+          <Button  title="Submit" onPress={(data:any) => handleSubmit(data)} />
+        </View>
+        <View style={{"marginBottom": 10}}>
+          <Button title="Close Modal" onPress={() => setCreateUserModalVisible(!createUserModalVisible)} />
+        </View>
+
+  </ScrollView>
+
+</Modal>
       </ThemedView>
     </ParallaxScrollView>
+    
   );
 }
 
@@ -56,4 +114,10 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
 });
